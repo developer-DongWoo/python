00301e78cff0b7adcc3912315bd0090474e136c0
developer-DongWoo <ehddn8547@gmail.com>
2023-05-09 18:38:51 +0900

    first

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Worlds!'
+    return "render_template('hello.html', name=name) << 와 같은 방식으로 html 파일을 client에게 전송합니다."
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = False
+    app.run(host='0.0.0.0')
