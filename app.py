2cd49b2b087e6f44d0d29affbdce645f4d0fac32
developer-DongWoo <ehddn8547@gmail.com>
2023-05-19 14:58:46 +0900

    second

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import pymysql
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,6 +21,45 @@ def postMember():
     payload = request.get_json()
     result = jsonify({"data":payload})
     return result
+
+
+class Model():
+    def getData(route,id):
+        try:
+            tableName=route
+            db = pymysql.connect(host='', port=3306, user='mysqlId', passwd='123123', db='mydb', charset='utf8')
+            cursor = db.cursor()
+            cursor.execute(f"SELECT * FROM {tableName} WHERE id={id}")
+            result = cursor.fetchall()
+            cursor.commit()
+            cursor.close()
+            return result
+        except:
+            pass
+
+    def getDatas(route,data):
+        try:
+            tableName=route
+            db = pymysql.connect(host='', port=3306, user='mysqlId', passwd='123123', db='mydb', charset='utf8')
+            cursor = db.cursor()
+            cursor.execute(f"SELECT * FROM {tableName}")
+            result = cursor.fetchall()
+            cursor.commit()
+            cursor.close()
+            return result
+        except:
+            pass
+
+    def postData(route,data):
+        pass
+
+    def patchData(route,data):
+        pass
+
+    def deleteData(route,data):
+        pass
+        
+    route, method, data
 
 if __name__ == '__main__':
     app.debug = False
